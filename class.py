f187ba808405c563ec162a9f83b22beea2a17bcf
sayk <97113955+saykdoteth@users.noreply.github.com>
2023-01-16 15:35:02 +0100

    ajout de la fonction soustraction

--- class.py
+++ class.py
@@ -1,11 +1,18 @@
+import unittest
+
 class SimpleMath:
     @staticmethod
     def addition(x, y):
         return x + y
-
-import unittest
+    
+    @staticmethod
+    def subtraction(x, y):
+        return x - y
 
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(2, 3)
         self.assertEqual(result, 5)
+    def test_subtraction(self):
+        result = SimpleMath.subtraction(5, 2)
+        self.assertEqual(result, 3)
